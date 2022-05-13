ccb14f27fc5779f05222894615d492406cffd9e4
carlos-mattos <ceamattos.jf@gmail.com>
2022-05-13 14:21:55 -0300

    io server and web frontend

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,20 @@
 import express from "express";
+import http from "node:http";
+import { Server } from "socket.io";
+import path from "node:path";
 
 const app = express();
 
-app
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, "..", "..", "web")));
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("Socket", socket);
+});
+
+server
   .listen(3000)
   .on("listening", () => console.log("Server is running on port 3000"));
