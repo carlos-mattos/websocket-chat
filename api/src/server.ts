5d3847e83aac43a10b738e0a8e2e8949b0f6251b
carlos-mattos <ceamattos.jf@gmail.com>
2022-05-14 11:13:09 -0300

    pull master

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import http from "node:http";
+import { Server } from "socket.io";
 
 const app = express();
 
-app
+const server = http.createServer(app);
+
+server
   .listen(3000)
   .on("listening", () => console.log("Server is running on port 3000"));
